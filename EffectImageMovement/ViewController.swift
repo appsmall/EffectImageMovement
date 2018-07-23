45e9e72fe80ef7b131722e41791c35d097fca524
Shashank Panwar <shashank.panwar@kayosys.com>
2018-07-23 17:30:14 +0530

    - ViewController -update code for parallex image

--- EffectImageMovement/ViewController.swift
+++ EffectImageMovement/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
